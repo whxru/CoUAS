39f63a621e0476364470cad11bab7a370e942908
whxru <seuhuangziyao@outlook.com>
2018-01-30 17:48:00 +0800

    Add explanation in comment

--- Monitor/user-module/mavc_example.js
+++ Monitor/user-module/mavc_example.js
@@ -13,50 +13,66 @@ class ExampleModule extends UserModule {
      * @memberof ExampleModule
      */
     constructor(name, droneCluster) {
+         // Call constructor of UserModule, it'll add an item to menu for this module
         super(name, droneCluster);
 
+        // Add command to menu
         this.addCommand('Console', () => {
+            // Print message on console.
             console.log("This is a message printed by ExampleModule");
         })
 
         this.addCommand('Input', () => {
+            // Create an dialog to wait for user's input and show it
             var inputSet = new wd.InputSet({
                 'title': 'Example Title',
-                'middle': true,
+                'middle': true, // vergical align
                 'modal': true
             }).showOnTop();
 
+            // Add a <select> element to the dialog
             var select = inputSet.addSelect(['select1', 'select2', 'select3']);
+            // Add an <input> element to the dialog
             var input = inputSet.addInput('Input', {
                 'type': 'number', 'min': '1', 'max': '50'
             });
+            // Add a <textarea> element to the dialog
             var textarea = inputSet.addTextarea({ rows: 8});
 
+            // Add a confirm button to the dialog
             var confirm = inputSet.addButton('Confirm', () => {
+                // Get value of elements
                 var selectedValue = select.options[select.selectedIndex].value;
                 var input_str = input.value;
                 var textarea_str = textarea.value;
+                // Remove this dialog
                 inputSet.remove()
+                // Print user's input on console
                 console.log('SELECTED: ' + selectedValue);
                 console.log('INPUT: ' + input_str);
                 console.log('TEXTAREA: ' + textarea_str);
             });
-
+            
+            // Add a cancel button to the dialog
             var cancel = inputSet.addButton('Cancel', () => {
+                // Remove this dialog
                 inputSet.remove();
             })
         })
 
+        // Handle every message sent by drone
         this.addMsgListener('message-in', (CID, msg_obj) => {
             console.log(JSON.stringify(msg_obj), 'green');
         });
-
+        
+        // Handle every message sent by monitor
         this.addMsgListener('message-out', (CID, msg_obj) => {
             console.log(JSON.stringify(msg_obj), 'pink');
         })
     }
 }
 
+// The init function will be called automatically while loading this module.
 function init(droneCluster) {
     new ExampleModule('example', droneCluster);
 }
