eb9cb33e3787ff89b1d3c94206a98dab0230aa5b
whxru <seuhuangziyao@outlook.com>
2018-03-19 11:59:35 +0800

    New methods add

--- Monitor/user-module/user_module.js
+++ Monitor/user-module/user_module.js
@@ -1,5 +1,5 @@
 const menu = require('../monitor-app/module/menu');
-
+const transform = require('../monitor/lib/transform');
 /**
  * Superclass of all user modules.
  * @class UserModule
@@ -29,7 +29,7 @@ class UserModule {
 
     /**
      * Get messages when the application receives/sends messages from/to RPi.
-     * @param {String} type - "message-in" or "message-out"
+     * @param {String} type - "message-in" or "message-out" or "new-drone-add"
      * @param {Function} listener - Handler of message.
      * @memberof UserModule
      */
@@ -58,29 +58,64 @@ class UserModule {
 
     /**
      * Send message to one single RPi.
-     * @param {Int} CID - Connection ID.
+     * @param {Number} CID - Connection ID.
      * @param {String} msg - Message to be sent.
      * @param {Function} callback - Callback function.
      * @memberof UserModule
      */
     sendMsg(CID, msg, callback) {
-        this.droneCluster.getDrone(CID).writeDataToPi(msg, callback);
+        var drone = this.droneCluster.getDrone(CID);
+        if(!drone) {
+            drone.writeDataToPi(msg, callback);
+        }
     }
 
     /**
      * Get the status of one single drone.
-     * @param {Int} CID - Connection ID
+     * @param {Number} CID - Connection ID
      * @returns {Object} An object which contains CID,Armd,Mode,Lat,Lon,Alt
      * @memberof UserModule
      */
     getStatus(CID) {
-        return this.droneCluster.getDrone(CID).getStatus();
+        var drone = this.droneCluster.getDrone(CID)
+        if(!drone) {
+            return drone.getStatus();
+        }
+        return null
+    }
+
+    /**
+     * Get the number of drones in the cluster.
+     * @returns {Number} Number of drones
+     * @memberof UserModule
+     */
+    getDroneNum() {
+        return this.droneCluster.getDroneNum();
+    }
+
+    /**
+     * Calculate the distance between two positions.
+     * @static
+     * @param {Object} pos1 - Object which contains attribute 'lat' and 'lng'
+     * @param {Object} pos2 - Object which contains attribute 'lat' and 'lng'
+     * @param {Boolean} [mars=false] - To determine the type of lat and lng
+     * @returns Distance in meters
+     * @memberof UserModule
+     */
+    static calDistance(pos1, pos2, mars=false) {
+        var marsPos1 = pos1,
+            marsPos2 = pos2;
+        if(!mars) {
+            marsPos1 = transform.wgs2gcj(pos1.lat,pos1.lng);
+            marsPos2 = transform.wgs2gcj(pos2.lat,pos2.lng);
+        }
+        return global.mapModule.calDistance(marsPos1, marsPos2);
     }
 }
 
 
 module.exports = {
     UserModule: UserModule,
-    console: require('../monitor-app/module/console'),
+    myConsole: require('../monitor-app/module/console'),
     wd: require('../monitor-app/module/window')
 }
