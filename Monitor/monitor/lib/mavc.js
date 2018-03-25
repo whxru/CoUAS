ae328846d79f3aa20d54005d8ca71d848a9ef942
whxru <seuhuangziyao@outlook.com>
2018-03-25 10:52:51 +0800

    New msg type for delay test added

--- Monitor/monitor/lib/mavc.js
+++ Monitor/monitor/lib/mavc.js
@@ -7,6 +7,8 @@ module.exports.MAVC = {
     "MAVC_ACTION": 4,             // Action to be performed
     "MAVC_ARRIVED": 5,            // Tell the monitor that the drone has arrived at the target
     "MAVC_DONE": 6,               // Close the connection between RPi and monitor 
+    "MAVC_DELAY_TEST": 101,       // To test the communication delay
+    "MAVC_DELAY_RESPONSE": 102,   // Response to MAVC_DELAY_TEST
     // Definitions of action type
     "ACTION_ARM_AND_TAKEOFF" : 0, // Ask drone to arm and takeoff
     "ACTION_GO_TO" : 1,           // Ask drone to fly to target specified by latitude and longitude
