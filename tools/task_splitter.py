3db4740d53708d2b272b782a2342f43a4754c17a
whxru <seuhuangziyao@outlook.com>
2018-07-11 19:58:18 +0800

    Support input directory path

--- tools/task_splitter.py
+++ tools/task_splitter.py
@@ -10,15 +10,13 @@ same directory with the task file. Those output files are distinguished by CID w
 
 import argparse
 import json
-import os
+from os import listdir
+from os.path import isfile, join
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", help="Path of the task file")
-    args = parser.parse_args()
 
+def split_task(file_path):
     each_drones_action = {}
-    with open(args.file_path, 'r') as task_file:
+    with open(file_path, 'r') as task_file:
         actions = json.loads(task_file.read())
         for action in actions:
             cid = action["CID"]
@@ -26,8 +24,24 @@ if __name__ == "__main__":
                 each_drones_action[cid] = []
             each_drones_action[cid].append(action)
 
-    prefix = args.file_path[:-5]
+    prefix = file_path[:-5]
     for cid, actions in each_drones_action.iteritems():
         output_file_path = prefix + "[CID={cid}].json".format(cid=cid)
         with open(output_file_path, "w+") as single_task_file:
             single_task_file.write(json.dumps(actions, sort_keys=True, indent=4))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="Path of the task file")
+    args = parser.parse_args()
+
+    path = args.path
+    if path.endswith(".json"):
+        split_task(path)
+    else:
+        task_file_paths = [join(path, file_name) for file_name in listdir(path) if isfile(join(path, file_name)) and
+                        file_name.endswith(".json")]
+        for file_path in task_file_paths:
+            split_task(file_path)
+
